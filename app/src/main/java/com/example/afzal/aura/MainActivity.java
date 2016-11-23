ac9bf27f94b8bbc2660142fc4f3392b3e47cb93d
HussainAfzal <hussain_afzal95@ymail.com>
2016-11-23 18:22:57 +0500

    Updated Main Activity

--- app/src/main/java/com/example/afzal/aura/MainActivity.java
+++ app/src/main/java/com/example/afzal/aura/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.afzal.aura;
 
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.accessibility.AccessibilityManager;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Date;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "AURA";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+        Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
+        startActivity(intent);
+
+        Intent startIntent = new Intent(MainActivity.this, BackgroundService.class);
+        startService(startIntent);
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
