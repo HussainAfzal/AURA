40837c49129d06e74bc1d90611bc3779690e46d4
HussainAfzal <hussain_afzal95@ymail.com>
2017-01-15 18:17:01 +0500

    FYP 1 changes

--- app/src/main/java/com/example/afzal/aura/BackgroundService.java
+++ app/src/main/java/com/example/afzal/aura/BackgroundService.java
@@ -1,13 +1,28 @@
 package com.example.afzal.aura;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.app.IntentService;
 import android.app.usage.UsageStats;
 import android.app.usage.UsageStatsManager;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Build;
+import android.os.Bundle;
 import android.os.Environment;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -19,6 +34,7 @@ import java.io.OutputStreamWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
@@ -28,6 +44,9 @@ import java.util.TreeMap;
 public class BackgroundService extends IntentService {
 
     File file;
+    private LocationManager locationMangaer = null;
+    private LocationListener locationListener = null;
+    private Boolean flag = false;
 
     public BackgroundService() {
         super("LogService");
@@ -46,7 +65,19 @@ public class BackgroundService extends IntentService {
 
         Log.d("AURA","File is created");
 
+
+        /*-------Location Coordinates work---------- */
+
+        locationMangaer = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
+
+        Location_log();
+
+        /*-------Location Work Complete-------------- */
+
         //Retrieving App Information
+
+        /*
 
         while(true) {
 
@@ -96,6 +127,35 @@ public class BackgroundService extends IntentService {
             Log.d("AURA","Wake Up!");
         }
 
+        */
+
+    }
+
+    void Location_log(){
+
+        Log.d("AURA","Into the Location Log");
+        flag = displayGpsStatus();
+        if (flag) {
+
+            locationListener = new MyLocationListener();
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            locationMangaer.requestLocationUpdates(LocationManager
+                    .GPS_PROVIDER, 5000, 10, locationListener);
+
+        } else {
+            Log.d("AURA","Your GPS is: OFF");
+        }
+
     }
 
     void createTextFile(){
@@ -107,5 +167,70 @@ public class BackgroundService extends IntentService {
         }
         file = new File(root, "log.txt");  // file path to save
 
+    }
+
+    /*----Method to Check GPS is enable or disable ----- */
+    private Boolean displayGpsStatus() {
+        ContentResolver contentResolver = getBaseContext()
+                .getContentResolver();
+        boolean gpsStatus = Settings.Secure
+                .isLocationProviderEnabled(contentResolver,
+                        LocationManager.GPS_PROVIDER);
+        if (gpsStatus) {
+            return true;
+
+        } else {
+            return false;
+        }
+    }
+
+    /*----------Listener class to get coordinates ------------- */
+    private class MyLocationListener implements LocationListener {
+        @Override
+        public void onLocationChanged(Location loc) {
+
+            Toast.makeText(getBaseContext(),"Location changed : Lat: " +
+                            loc.getLatitude()+ " Lng: " + loc.getLongitude(),
+                    Toast.LENGTH_SHORT).show();
+            String longitude = "Longitude: " +loc.getLongitude();
+            Log.v("AURA", longitude);
+            String latitude = "Latitude: " +loc.getLatitude();
+            Log.v("AURA", latitude);
+
+    /*----------to get City-Name from coordinates ------------- */
+            String cityName=null;
+            Geocoder gcd = new Geocoder(getBaseContext(),
+                    Locale.getDefault());
+            List<Address>  addresses;
+            try {
+                addresses = gcd.getFromLocation(loc.getLatitude(), loc
+                        .getLongitude(), 1);
+                if (addresses.size() > 0)
+                    System.out.println(addresses.get(0).getLocality());
+                cityName=addresses.get(0).getLocality();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            String s = longitude+"\n"+latitude +
+                    "\n\nMy Currrent City is: "+cityName;
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public void onStatusChanged(String provider,
+                                    int status, Bundle extras) {
+            // TODO Auto-generated method stub
+        }
     }
 }
--- app/src/main/java/com/example/afzal/aura/MainActivity.java
+++ app/src/main/java/com/example/afzal/aura/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.afzal.aura;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.PendingIntent;
 import android.app.usage.UsageStats;
 import android.app.usage.UsageStatsManager;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Environment;
 import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.accessibility.AccessibilityManager;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -34,16 +46,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
 
-        Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
-        startActivity(intent);
-
-        Intent startIntent = new Intent(MainActivity.this, BackgroundService.class);
-        startService(startIntent);
-
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Intent accessUsageSetting = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
+        startActivityForResult(accessUsageSetting,1);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d("AURA","In onActivityResult");
+        Log.d("AURA","requestcode: "+requestCode+"  resultCode:"+resultCode);
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            startActivityForResult(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS),2);
+        }
+        if (requestCode == 2) {
+            startService(new Intent(MainActivity.this,ForegroundService.class));
+        }
     }
 }
